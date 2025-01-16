a0dbfe3f6571a70cba65c39869ad17e4d8a67998
Dragoner91 <Dragoner919@hotmail.com>
2025-01-15 22:18:56 -0400

    feat: :fix: ui fix in combo horizontal listview

--- lib/presentation/widgets/combo/combo_horizontal_listview.dart
+++ lib/presentation/widgets/combo/combo_horizontal_listview.dart
@@ -117,183 +117,188 @@ class _SlideState extends ConsumerState<_Slide> {
     final isInCart = checkIfIsInCart();
 
     final textStyles = Theme.of(context).textTheme;
+    final theme = Theme.of(context);
+    final primaryColor = theme.colorScheme.shadow;
 
     return Container(
       margin: const EdgeInsets.symmetric(horizontal: 8),
       decoration: BoxDecoration(
+        //color: backgroundColor,
         borderRadius: const BorderRadius.all(Radius.circular(10)),   
-        border: Border.all(color: const Color.fromARGB(136, 186, 186, 186)),
+        border: Border.all(color: Colors.transparent),
         shape: BoxShape.rectangle,
       ),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           //*imagen
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(Radius.circular(10)),        
-              border: Border.all(color: const Color.fromARGB(136, 186, 186, 186)),
-              shape: BoxShape.rectangle,
-            ),
-            child: SizedBox(
-              width: 150,
-              child: Stack(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(20),
-                    child: GestureDetector(
-                      onTap: () {
-                        //*colocar el id en el provider de currentProduct
-                        ref.read(currentCombo.notifier).update((state) => [...state, widget.combo] ); //*arreglar
-                        ref.read(currentStateNavBar.notifier).update((state) => -1);
-                        context.push("/combo");
-                      },
-                      child: Image.network(
-                        widget.combo.imageUrl[0],  //*siempre se visualiza la primera imagen del arreglo de imagenes
-                        fit: BoxFit.contain,
-                        height: 150,
-                        width: 150,
-                        loadingBuilder: (context, child, loadingProgress) {
-                          if( loadingProgress != null){
-                            return const Center(
-                              child: SizedBox(
-                                height: 150,
-                                width: 150,
-                                child: Padding(
-                                  padding: EdgeInsets.all(45),
-                                  child: CircularProgressIndicator(
-                                    strokeWidth: 2, 
-                                    color: Color(0xFF5D9558),
-                                  ),
+          SizedBox(
+            width: 150,
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: GestureDetector(
+                    onTap: () {
+                      //*colocar el id en el provider de currentProduct
+                      ref.read(currentCombo.notifier).update((state) => [...state, widget.combo] ); //*arreglar
+                      ref.read(currentStateNavBar.notifier).update((state) => -1);
+                      context.push("/combo");
+                    },
+                    child: Image.network(
+                      widget.combo.imageUrl[0],  //*siempre se visualiza la primera imagen del arreglo de imagenes
+                      fit: BoxFit.contain,
+                      height: 150,
+                      width: 150,
+                      loadingBuilder: (context, child, loadingProgress) {
+                        if( loadingProgress != null){
+                          return const Center(
+                            child: SizedBox(
+                              height: 150,
+                              width: 150,
+                              child: Padding(
+                                padding: EdgeInsets.all(45),
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 2, 
+                                  color: Color(0xFF5D9558),
                                 ),
                               ),
-                            );
-                          }
-                          return FadeIn(child: child);
-                        },
-                                  
-                      ),
+                            ),
+                          );
+                        }
+                        return FadeIn(child: child);
+                      },
+                                
                     ),
                   ),
-                  const SizedBox(height: 100,),
-                  Row(
-                    children: [
-                      const Spacer(),
-                      SizedBox(
-                        width: 45,
-                        height: 45,
-                        child: Padding(
-                          padding: const EdgeInsets.all(6),
-                          child: FutureBuilder<bool>(
-                            future: isInCart,
-                            builder: (context, snapshot) {
-                              final inCart = snapshot.hasData && snapshot.data == true;
-                              return IconButton(
-                                color: Colors.white,
-                                style: ButtonStyle(
-                                  backgroundColor: WidgetStateProperty.all(snapshot.data == true ? Colors.black54 : const Color(0xFF5D9558)),
-                                ),
-                                onPressed: inCart ? null : () async {
-                                  //*agregar producto al carrito
-                                  final cart = ref.watch(cartItemsProvider.notifier).addItemToCart;
-                                  cart(CartItemMapper.cartItemToEntity(CartLocal.fromEntity(widget.combo, 1, widget.combo.imageUrl[0], "Combo")));
-                                },
-                                icon: inCart ? const Icon(Icons.check, size: 14, color: Colors.white,) : const Icon(Icons.add, size: 14, color: Colors.white,),
-                              );
-                            },
-                          ),
+                ),
+                const SizedBox(height: 100,),
+                Row(
+                  children: [
+                    const Spacer(),
+                    SizedBox(
+                      width: 45,
+                      height: 45,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FutureBuilder<bool>(
+                          future: isInCart,
+                          builder: (context, snapshot) {
+                            final inCart = snapshot.hasData && snapshot.data == true;
+                            return IconButton(
+                              color: Colors.white,
+                              style: ButtonStyle(
+                                backgroundColor: WidgetStateProperty.all(snapshot.data == true ? Colors.black54 : const Color(0xFF5D9558)),
+                              ),
+                              onPressed: inCart ? null : () async {
+                                //*agregar producto al carrito
+                                final cart = ref.watch(cartItemsProvider.notifier).addItemToCart;
+                                cart(CartItemMapper.cartItemToEntity(CartLocal.fromEntity(widget.combo, 1, widget.combo.imageUrl[0], "Combo")));
+                              },
+                              icon: inCart ? const Icon(Icons.check, size: 14, color: Colors.white,) : const Icon(Icons.add, size: 14, color: Colors.white,),
+                            );
+                          },
                         ),
                       ),
-                    ],
-                  ),
-                  widget.discount > 0 
-                  ? Row(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: Colors.amber.shade600.withOpacity(0.75)
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(HumanFormarts.percentage(widget.discount), style: const TextStyle(fontWeight: FontWeight.bold),),
-                          ),
+                    ),
+                  ],
+                ),
+                widget.discount > 0 
+                ? Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.amber.shade600.withOpacity(0.75)
                         ),
-                      )
-                    ],
-                  )
-                  : const SizedBox.shrink(),
-                ],
-              ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(HumanFormarts.percentage(widget.discount), style: const TextStyle(fontWeight: FontWeight.bold),),
+                        ),
+                      ),
+                    )
+                  ],
+                )
+                : const SizedBox.shrink(),
+              ],
             ),
           ),
           const SizedBox(
             height: 5,
           ),
-          //*title
-          SizedBox(
-            width: 150,
-            child: Row(
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 2),
+            decoration: BoxDecoration(
+              borderRadius: const BorderRadius.all(Radius.circular(10)),   
+              border: Border.all(color: primaryColor),
+              shape: BoxShape.rectangle,
+            ),
+            child: Column(
               children: [
-                const SizedBox(width: 5,),
-                Expanded(
-                  child: Text(
-                    widget.combo.name,
-                    maxLines: 1,
-                    style: textStyles.bodyLarge,
+                //*title
+                SizedBox(
+                  width: 150,
+                  child: Row(
+                    children: [
+                      const SizedBox(width: 5,),
+                      Expanded(
+                        child: Text(
+                          widget.combo.name, //*arreglar cuando este producto listo
+                          maxLines: 1,
+                          style: textStyles.bodyLarge,
+                        ),
+                      ),
+                      const SizedBox(width: 5,),
+                    ],
                   ),
                 ),
-                const SizedBox(width: 5,),
+                
+                //*price
+                SizedBox(
+                  width: 150,
+                  child: Row( //*arreglar cuando este producto listo
+                    children: [
+                    
+                      const SizedBox(width: 5,),
+                      if (widget.discount > 0) ...[
+                        Text(
+                          "${HumanFormarts.numberCurrency(widget.combo.price)} ${widget.combo.currency}",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 14,
+                            color: Colors.red,
+                            decoration: TextDecoration.lineThrough,
+                            decorationColor: Colors.red,
+                          ),
+                        ),
+                        const SizedBox(width: 5,),
+                        Text(
+                          "${HumanFormarts.numberCurrency(widget.combo.price - ( widget.combo.price * widget.discount))} ${widget.combo.currency}",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ] else ...[
+                        Text(
+                          "${HumanFormarts.numberCurrency(widget.combo.price)} ${widget.combo.currency}",
+                          style: const TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ],
+                      const SizedBox(width: 5,), 
+                    ],
+                  ),
+                )
               ],
             ),
           ),
-
-          //*price
-          SizedBox(
-            width: 150,
-            child: Row( //*arreglar cuando este producto listo
-              children: [
-
-                const SizedBox(width: 5,),
-                if (widget.discount > 0) ...[
-                  Text(
-                    "${HumanFormarts.numberCurrency(widget.combo.price)} ${widget.combo.currency}",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 14,
-                      color: Colors.red,
-                      decoration: TextDecoration.lineThrough,
-                      decorationColor: Colors.red,
-                    ),
-                  ),
-                  const SizedBox(width: 5,),
-                  Text(
-                    "${HumanFormarts.numberCurrency(widget.combo.price - ( widget.combo.price * widget.discount))} ${widget.combo.currency}",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                ] else ...[
-                  Text(
-                    "${HumanFormarts.numberCurrency(widget.combo.price)} ${widget.combo.currency}",
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
-                const SizedBox(width: 5,), 
-              ],
-            ),
-          )
-
-
-
-
-      ]),
+        ]
+      ),
     );
   }
 }
